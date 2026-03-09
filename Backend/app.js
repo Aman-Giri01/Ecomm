94c72d461b5d6afd0c5c6520aa686f5e7a991413
Aman-Giri01 <amankumargiri2022@gmail.com>
2026-03-09 20:08:48 +0530

    mongo atlas

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-   origin: "http://localhost:5173"||process.env.CLIENT_URL  ,
+   origin: process.env.CLIENT_URL || "http://localhost:5173"  ,
     credentials: true,
   })
 );
